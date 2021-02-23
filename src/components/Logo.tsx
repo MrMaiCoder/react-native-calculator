256da9763dd618b446d404a978aa5a2e6c5662a0
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-02-23 12:41:01 +0700

    Changed app logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { Image, StyleSheet } from 'react-native';
 
 const Logo = () => (
-  <Image source={require('../assets/logo.png')} style={styles.image} />
+  <Image source={require('../assets/logo-calculator.png')} style={styles.image} />
 );
 
 const styles = StyleSheet.create({
@@ -10,6 +10,7 @@ const styles = StyleSheet.create({
     width: 128,
     height: 128,
     marginBottom: 12,
+
   },
 });
 
